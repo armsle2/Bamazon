e1aa1da3e1dc083ea73d0e584ced0a5306c2bd56
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-01-09 19:47:32 -0500

    created prompt to allow user to select product and quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-
+const inquirer = require('inquirer');
 const connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
@@ -11,17 +11,51 @@ const connection = mysql.createConnection({
 connection.connect(err=>{
 	if(err) throw err;
 	console.log(`connected`);
-	displayAllItems()
+	displayAllItems(askUser);
 });
 
-let displayAllItems = ()=>{
+let displayAllItems = (cb)=>{
 	connection.query('SELECT item_id, product_name, price_usd FROM products', (err,results)=>{
 		if(err){
 			console.log(err)
 		}else{
 			results.forEach((results, index)=>{
 				console.log('Item: '+results.item_id +'\n'+ 'Product: '+results.product_name+'\n'+ 'Price: $'+results.price_usd+'\n-----------------');
-			})			
+			});
+			cb(results);
 		}
-	})
+	});
 }
+
+let askUser = (items)=>{
+	inquirer.prompt([
+	{
+		name: 'item_id',
+		type: 'list',
+		choices: ()=>{
+			let itemsArr = [];
+			items.forEach((results, index)=>{
+				itemsArr.push(`${results.item_id} ${results.product_name}`);
+			});
+			return itemsArr;
+		},
+		message: 'Please select the product you would like to buy.'
+	}, {
+		name: 'quantity',
+		type: 'input',
+		message: 'How many would you like to buy?',
+		validate: (value)=>{
+			if(!isNaN(value)){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+	])
+}
+
+// let checkInventory = (item, quantity)=>{
+// 	connection.query('SELECT'
+// 		)
+// }
